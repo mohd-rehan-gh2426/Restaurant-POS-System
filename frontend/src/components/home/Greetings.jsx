adcad7d9f63f051fe3a999b631b30f307576a450
Mohd Rehan <mdrehanchhipa@gmail.com>
2025-09-16 22:55:08 +0530

    designed MiniCards

---
+++ frontend/src/components/home/Greetings.jsx
@@ -0,0 +1,55 @@
+import React, { useEffect } from "react";
+import { useState } from "react";
+
+const Greetings = () => {
+  const [dateTime, setDateTime] = useState(new Date());
+
+  useEffect(() => {
+    const timer = setInterval(() => setDateTime(new Date()), 1000);
+     return () => clearInterval(timer);
+  }, []);
+
+  const formatDate = (date) => {
+    const months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    return `${months[date.getMonth()]} ${String(date.getDate()).padStart(
+      2,
+      "0"
+    )}, ${date.getFullYear()}`;
+  };
+
+  const formatTime = (date) =>
+    `${String(date.getHours()).padStart(2, "0")}:${String(
+      date.getMinutes()
+    ).padStart(2, "0")}:${String(date.getSeconds()).padStart(2, "0")}`;
+  return (
+    <div className="flex justify-between items-center px-8 mt-5">
+      <div>
+        <h1 className="text-[#f5f5f5] text-2xl font-semibold tracking-wide">
+          Good Morning, Rehan
+        </h1>
+        <p className="text-[#ababab] text-sm">
+          Welcome back to your dashboard!
+        </p>
+      </div>
+      <div>
+        <h1 className="text-[#f5f5f5] text-3xl font-bold tracking-wide w-[130px]">{formatTime(dateTime)}</h1>
+        <p className="text-[#ababab] text-sm">{formatDate(dateTime)}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Greetings;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,19 +1,41 @@
-import React from 'react'
-import BottomNav from '../components/shared/BottomNav'
+import React from "react";
+import BottomNav from "../components/shared/BottomNav";
+import Greetings from "../components/home/Greetings";
+import MiniCard from "../components/home/MiniCard";
+import { BsCashCoin } from "react-icons/bs";
+import { GrInProgress } from "react-icons/gr";
+import RecentOrders from "../components/home/RecentOrders";
 
 const Home = () => {
   return (
-    <section className='bg-[#1f1f1f] h-[calc(100vh-5rem)] overflow-hidden flex'>
-        {/* {LEFT DIV} */}
-        <div className='flex-[3] bg-red-600'></div>
+    <section className="bg-[#1f1f1f] h-[calc(100vh-5rem)] overflow-hidden flex">
+      {/* {LEFT DIV} */}
+      <div className="flex-[3] ">
+        <Greetings />
+        <div className="flex items-center w-full gap-3 px-8 mt-8">
+          <MiniCard
+            title={"Total Earnings"}
+            icon={<BsCashCoin />}
+            number={512}
+            footerNum={1.6}
+          />
+          <MiniCard
+            title={"In Progress"}
+            icon={<GrInProgress />}
+            number={16}
+            footerNum={3.6}
+          />
+        </div>
+        <RecentOrders/>
+      </div>
 
-        {/* {RIGHT DIV} */}
-        <div className='flex-[2] bg-blue-600'></div>
+      {/* {RIGHT DIV} */}
+      <div className="flex-[2] bg-blue-600"></div>
 
-        {/* {BOTTOM NAV} */}
-        <BottomNav/>
+      {/* {BOTTOM NAV} */}
+      <BottomNav />
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
